cab002f82aeef594dd3d06a026e98551204ba89d
Janet Huaman Alvarez <i201918044@cibertec.edu.pe>
2024-10-06 08:56:46 -0500

    Proyecto Hilanderia

---
+++ src/main/java/com/example/hialpesa_frontend_exportacion/HialpesaFrontendExportacionApplication.java
@@ -0,0 +1,13 @@
+package com.example.hialpesa_frontend_exportacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HialpesaFrontendExportacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HialpesaFrontendExportacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hialpesa_frontend_exportacion/HialpesaFrontendExportacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hialpesa_frontend_exportacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HialpesaFrontendExportacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
